1c15634c97d8135a9a601e79c0bc532b61d224f8
Nick Teleky <nteleky@gmail.com>
2013-02-14 12:25:56 -0500

    Adds Ms. PacMan to board

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -6,7 +6,11 @@ function draw() {
         ctx = canvas.getContext('2d');
         board = new Image();
         board.src = 'pacman10-hp-sprite.png';
+        //Draws just the blue board on the scren.
         ctx.drawImage(board, 322, 3, 463, 138, 0, 0, 463, 138);
+        mspm = new Image();
+        mspm.src = 'pacman10-hp-sprite.png';
+        ctx.drawImage(mspm, 121, 4, 19, 19, 224, 93, 19, 19);
     } else {
         alert("Epic fail. This doesn't work. Get a better browser.")
     }
