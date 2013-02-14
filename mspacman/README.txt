642db2a965ac72644da7d8af047f6f42bd3f37e0
Nick Teleky <nteleky@gmail.com>
2013-02-14 12:44:36 -0500

    Completed lab 6

---
+++ mspacman/README.txt
@@ -0,0 +1,12 @@
+Comp20 - Web Programming
+Lab 6: Ms. Pac-Man
+Feb 14, 2012
+Programmer: Nick Teleky
+
+
+ -- All parts of Lab 6 correctly implemented. Google PacMan board renders with
+ a Ms. PacMan in the center, being chased by a blue ghost. All images are
+ rendered from the one given sprite image.
+
+ Time to implement: About 2 hours (most of that time spent looking up functions
+ and finding dimensions within the sprite image).
--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -1,16 +1,21 @@
-function draw() {
+function drawPacMan() {
     canvas = document.getElementById('pacman');
 
     //Check that canvas works on browser
     if (canvas.getContext) {
         ctx = canvas.getContext('2d');
+        //Draws the blue board on the canvas.
         board = new Image();
         board.src = 'pacman10-hp-sprite.png';
-        //Draws just the blue board on the scren.
         ctx.drawImage(board, 322, 3, 463, 138, 0, 0, 463, 138);
+        //Draws a Ms. PacMan to the canvas, on the board.
         mspm = new Image();
         mspm.src = 'pacman10-hp-sprite.png';
         ctx.drawImage(mspm, 121, 4, 19, 19, 224, 93, 19, 19);
+        //Draws a blue ghost on the canvas.
+        ghost = new Image();
+        ghost.src = 'pacman10-hp-sprite.png';
+        ctx.drawImage(ghost, 120, 122, 19, 19, 180, 92, 19, 19);
     } else {
         alert("Epic fail. This doesn't work. Get a better browser.")
     }
