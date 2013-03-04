6cb901b42e0870ca7c0ce3de4c2d829d3f1fec1a
Nick Teleky <nteleky@gmail.com>
2013-03-03 22:47:47 -0500

    Have first four T stations and polyline on map; added images for icons

--- where/where.js
+++ where/where.js
@@ -2,11 +2,49 @@
 /*
  * 
  */
+
+//Declare global variables
+    var map;
+    var redStations = [];
+    var markers = [];
+    var mapOptions = [];
+
 function loadMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(39.027719, -75.289307),
-        zoom: 9,
+    mapOptions = {
+        center: new google.maps.LatLng(42.330497742, -71.095794678),
+        zoom: 11,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    loadRedLine();
 }
+
+function loadRedLine() {
+    tlogo = "images/tlogo.png";
+    
+    pt = new google.maps.LatLng(42.395428, -71.142483);
+    markers.push(new google.maps.Marker({position: pt, map: map, title: "Alewife", icon: tlogo}));
+    redStations.push(pt);
+
+    pt = new google.maps.LatLng(42.39674, -71.121815);
+    markers.push(new google.maps.Marker({position: pt, map: map, title: "Davis", icon: tlogo}));
+    redStations.push(pt);
+    pt = new google.maps.LatLng(42.3884, -71.119149);
+    markers.push(new google.maps.Marker({position: pt, map: map, title: "Porter Square", icon: tlogo}));
+    redStations.push(pt);
+    pt = new google.maps.LatLng(42.373362, -71.118956);
+    markers.push(new google.maps.Marker({position: pt, map: map, title: "Harvard Square", icon: tlogo}));
+    redStations.push(pt);
+
+    // Create polyline for Red Line Subway Stops
+    redLine = new google.maps.Polyline({
+        path: redStations,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1.0,
+        strokeWeight: 10
+    });
+    redLine.setMap(map);
+
+
+}
+
