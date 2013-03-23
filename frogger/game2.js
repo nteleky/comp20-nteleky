c8bbcb1546de6ac6868b6c64461b57edf8eeb93d
Nick Teleky <nteleky@gmail.com>
2013-03-23 04:00:08 -0400

    Added draw, move, isValidMove, and setDir funcs for frog

--- frogger/game2.js
+++ frogger/game2.js
@@ -6,12 +6,17 @@
 var sprites;
 var frogX, frogY; //X, Y coordinates for the frog.
 var frog = new Object();
+var block = {
+    width: 30,
+    height: 35
+};
 
 //Starts game when document is ready
 $(document).ready(function() {
     init_vars();
     sprites.onload = function () {
         draw_gameboard();
+        frog.draw();
     };
 });
 
@@ -19,7 +24,31 @@ $(document).ready(function() {
 function init_vars() {
     sprites = new Image();
     sprites.src = 'assets/frogger_sprites.png';
-    //frog d
+    //frog definition w/starting variables.
+    frog = {
+        x: 180,
+        y: 490,
+        dir: "forward",
+        draw: function() {
+            pencil.drawImage(sprites, 10, 367, 25, 25, this.x, this.y, 25, 25);
+        },
+        move: function() {
+            if (this.dir == "forward") this.y = this.y-block.height;
+            else if (this.dir == "backward") this.y = this.y+block.height
+        },
+        isValidMove: function() {
+            if (this.dir == "forward") if (this.y-block.height >= 98) return true;
+            else if (this.dir == "backward") if (this.y+block.height <= 520) return true;
+            else if (this.dir == "left") if (this.x-block.width >= 0) return true;
+            else if (this.dir == "right") if (this.x+block.width <= 399) return true;
+
+            return false;
+        },
+        setDirection: function(direction) {
+            this.dir = direction;
+        }
+
+    }
 
 }
 
@@ -35,7 +64,7 @@ function draw_gameboard() {
         //Splices FROGGER title
         pencil.drawImage(sprites, 11, 9, 329, 37, 5, 5, 329, 37);
         //Splices grass
-         pencil.drawImage(sprites, 0, 53, 399, 59, 0, 50, 399, 59);
+         pencil.drawImage(sprites, 0, 53, 399, 59, -3, 45, 399, 59);
         //Splices purple 'safe zones'
         pencil.drawImage(sprites, 0, 116, 399, 40, 0, 270, 399, 40);
         pencil.drawImage(sprites, 0, 116, 399, 40, 0, 480, 399, 40);    }
